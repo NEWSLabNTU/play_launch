c3cebf3180b29c5a3280dc5bb9872cc266080e8e
aeon <jerry73204@gmail.com>
2026-02-21 12:31:57 +0800

    Bump to 0.6.0

--- python/play_launch/__init__.py
+++ python/play_launch/__init__.py
@@ -4,7 +4,7 @@ play_launch - ROS2 Launch Inspection Tool
 Records and replays ROS 2 launch file executions for performance analysis.
 """
 
-__version__ = "0.5.1"
+__version__ = "0.6.0"
 
 from play_launch.cli import main
 
