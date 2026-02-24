31c843e59dff3af5669d4c75e328f6158d383ff7
aeon <jerry73204@gmail.com>
2026-02-24 12:40:16 +0800

    Remove unused dead code

--- src/play_launch/src/ros/launch_dump.rs
+++ src/play_launch/src/ros/launch_dump.rs
@@ -1,12 +1,9 @@
-use itertools::{chain, Itertools};
 use serde::{Deserialize, Serialize};
 use std::{
-    borrow::Cow,
     collections::HashMap,
     fs::File,
     io::BufReader,
     path::{Path, PathBuf},
-    process::Command,
 };
 
 pub type ParameterValue = String;
@@ -105,154 +102,6 @@ pub struct ComposableNodeRecord {
 
     #[allow(dead_code)]
     pub env: Option<Vec<(String, String)>>,
-}
-
-impl ComposableNodeRecord {
-    fn to_cmdline(&self, standalone: bool) -> Vec<String> {
-        if standalone {
-            self.to_cmdline_standalone()
-        } else {
-            self.to_cmdline_component()
-        }
-    }
-
-    fn to_cmdline_standalone(&self) -> Vec<String> {
-        let Self {
-            package: _,
-            plugin: _,
-            namespace,
-            log_level,
-            remaps,
-            params: _,
-            extra_args: _,
-            node_name,
-            target_container_name: _,
-            env: _,
-        } = self;
-
-        // Option A: Spawn standalone container directly using ament index
-        // Find component_container executable directly
-        let container_exe = match crate::ros::ament_index::find_executable(
-            "rclcpp_components",
-            "component_container",
-        ) {
-            Ok(path) => path.to_string_lossy().into_owned(),
-            Err(_) => {
-                // Fallback to full path if ament index fails
-                "/opt/ros/humble/lib/rclcpp_components/component_container".to_string()
-            }
-        };
-
-        // Generate unique container name for this standalone node
-        let container_name = format!("standalone_container_{}", node_name);
-        let node_remap = format!("__node:={}", container_name);
-        let ns_remap = format!("__ns:={}", namespace);
-
-        let command = vec![
-            Cow::from(container_exe),
-            Cow::from("--ros-args"),
-            Cow::from("-r"),
-            Cow::from(node_remap),
-            Cow::from("-r"),
-            Cow::from(ns_remap),
-        ];
-
-        let remap_args = remaps
-            .iter()
-            .flat_map(|(src, tgt)| [Cow::from("-r"), format!("{src}:={tgt}").into()]);
-
-        let log_level_args = log_level
-            .as_deref()
-            .map(|level| vec![Cow::from("--log-level"), Cow::from(level)])
-            .unwrap_or_default();
-
-        // Note: Standalone mode now only spawns the container.
-        // Component loading would need to be done separately via component_loader,
-        // which is a more complex refactoring. For now, this provides the container
-        // infrastructure for manual component loading.
-        chain!(command, log_level_args, remap_args)
-            .map(|arg| arg.to_string())
-            .collect()
-    }
-
-    fn to_cmdline_component(&self) -> Vec<String> {
-        let Self {
-            package,
-            plugin,
-            namespace,
-            log_level,
-            remaps,
-            params,
-            extra_args,
-            target_container_name,
-            node_name,
-            env: _,
-        } = self;
-
-        let command = [
-            "ros2",
-            "component",
-            "load",
-            target_container_name,
-            package,
-            plugin,
-            "-n",
-            node_name,
-            "--node-namespace",
-            namespace,
-        ]
-        .into_iter()
-        .map(Cow::from);
-
-        let remap_args = remaps
-            .iter()
-            .flat_map(|(src, tgt)| [Cow::from("-r"), format!("{src}:={tgt}").into()]);
-
-        let param_args = params
-            .iter()
-            .flat_map(|(name, value)| [Cow::from("-p"), format!("{name}:={}", value).into()]);
-
-        let extra_arg_args = extra_args
-            .iter()
-            .flat_map(|(name, value)| [Cow::from("-e"), format!("{name}:={}", value).into()]);
-
-        let log_level_args = log_level
-            .as_deref()
-            .map(|level| ["--log-level", level])
-            .into_iter()
-            .flatten()
-            .map(Cow::from);
-
-        chain!(
-            command,
-            log_level_args,
-            remap_args,
-            param_args,
-            extra_arg_args
-        )
-        .map(|arg| arg.to_string())
-        .collect()
-    }
-
-    pub fn to_command(&self, standalone: bool) -> Command {
-        let cmdline = self.to_cmdline(standalone);
-        let (program, args) = cmdline
-            .split_first()
-            .expect("command line must not be empty");
-        let mut command = Command::new(program);
-        command.args(args);
-        command
-    }
-
-    pub fn to_shell(&self, standalone: bool) -> Vec<u8> {
-        let cmdline = self.to_cmdline(standalone);
-        Itertools::intersperse(
-            cmdline.into_iter().map(|w| shell_quote::Sh::quote_vec(&w)),
-            vec![b' '],
-        )
-        .flatten()
-        .collect()
-    }
 }
 
 /// Read an deserialize the launch record dump.
